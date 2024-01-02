5bf8a14e4007ac675e0fb219982392379a0d7e95
Lukebray <39488446+Lukebray@users.noreply.github.com>
2024-01-02 15:55:07 +0000

    attack working

--- scenes/entities/player.gd
+++ scenes/entities/player.gd
@@ -4,15 +4,36 @@ extends CharacterBody2D
 
 @onready var animation_player = $AnimationPlayer
 @onready var sprite_2d = $Sprite2D
+@onready var weapon = $Weapon as Node2D
 
 var previous_movement_direction : Vector2
+var is_attacking : bool
+
+func _ready():
+	is_attacking = false
+	previous_movement_direction = Vector2.DOWN
+	weapon.visible = false
+
 
 func _physics_process(_delta):
 	handle_input()
 	move_and_slide()
-	update_movement_animation()
+	update_animation()
 
-func update_movement_animation():
+
+func handle_input():
+	var move_direction = Input.get_vector("left", "right", "up", "down")
+	if move_direction:
+		previous_movement_direction = move_direction
+	velocity = move_direction * movement_speed
+	
+	if Input.is_action_just_pressed("attack"):
+		update_attack_animation()
+
+
+func update_animation():
+	if is_attacking:
+		return
 	# IDLE
 	if velocity == Vector2.ZERO:
 		if previous_movement_direction == Vector2.UP:
@@ -27,11 +48,9 @@ func update_movement_animation():
 			if sprite_2d.flip_h == false:
 				sprite_2d.flip_h = true
 			animation_player.play("idle_side")
-		else:
-			animation_player.play("idle_front")
 	
 	# MOVING
-	if velocity.x < 0:
+	if velocity.x < 0: # moving right
 		if sprite_2d.flip_h == false:
 			sprite_2d.flip_h = true
 		animation_player.play("walk_right") # reverse this since it's walking left
@@ -45,8 +64,32 @@ func update_movement_animation():
 		animation_player.play("walk_up")
 
 
-func handle_input():
-	var move_direction = Input.get_vector("left", "right", "up", "down")
-	if move_direction:
-		previous_movement_direction = move_direction
-	velocity = move_direction * movement_speed
+func update_attack_animation():
+	is_attacking = true
+	weapon.visible = true
+	if velocity == Vector2.ZERO:
+		if previous_movement_direction == Vector2.UP:
+			animation_player.play("attack_up")
+		elif previous_movement_direction == Vector2.DOWN:
+			animation_player.play("attack_down")
+		elif previous_movement_direction == Vector2.RIGHT:
+			animation_player.play("attack_right")
+		elif previous_movement_direction == Vector2.LEFT:
+			animation_player.play("attack_left")
+
+	if velocity.y < 0: # moving up
+		animation_player.play("attack_up")
+	elif velocity.y > 0: # moving down
+		animation_player.play("attack_down")
+	elif velocity.x > 0: # moving right
+		animation_player.play("attack_right")
+	elif velocity.x < 0: # moving left
+		animation_player.play("attack_left")
+		
+	await animation_player.animation_finished
+	is_attacking = false
+	weapon.visible = false
+
+
+
+
